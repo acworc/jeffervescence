06db0522fe716e013b211704f463e5f1d596e230
Andrew Worcester <worcesac@rose-hulman.edu>
2017-06-08 11:23:26 -0400

    Changed code to David's implementation to help follow along with lessons, old Jeff site is available at acworc.github.io

--- app.js
+++ app.js
@@ -1,111 +1,57 @@
 const app = {
-    init(selectors) {
-        this.max = 0
-        this.list = document.querySelector(selectors.listSelector)
-        this.storedFlicks = []
-        document
-            .querySelector(selectors.formSelector)
-            .addEventListener('submit', this.addFlick.bind(this))
-        document.querySelector(selectors.swapSelector).addEventListener('submit', this.handleSwap.bind(this))
-    },
-    
-    addFlick(ev) {
-        ev.preventDefault()
+  init(selectors) {
+    this.flicks = []
+    this.max = 0
+    this.list = document
+      .querySelector(selectors.listSelector)
+    this.template = document
+      .querySelector(selectors.templateSelector)
+    document
+      .querySelector(selectors.formSelector)
+      .addEventListener('submit', this.addFlick.bind(this))
+  },
 
-        const f = ev.target
-        const flick = {
-            id: this.max + 1,
-            name: f.flickName.value,
-        }
-        
-        const listItem = this.renderListItem(flick)
-        // this.list.appendChild(listItem)
-        this.storedFlicks.push(listItem.textContent)
-        this.reloadList()
-        this.max++
-    },
-
-    renderListItem(flick) {
-        const item = document.createElement('li')
-        item.textContent = flick.name
-        return item
-    },
-    reloadList() {
-        this.list.innerHTML = ''
-        for (let i = 0; i < this.storedFlicks.length; i++) {
-            this.list.appendChild(this.createLI(this.storedFlicks[i], i))
-
-            document.querySelector(`#s${i}`).addEventListener("click", this.handleDelete.bind(this))
-            document.querySelector(`#h${i}`).addEventListener("click", this.handleHighlight.bind(this))
-        }
-    },
-
-    createLI(content, number) {
-        const li = document.createElement('li')
-
-        const send = document.createElement('button')
-        send.setAttribute("id", "s" + number)
-        send.textContent = "Delete"
-        li.appendChild(send)
-
-        const highlight = document.createElement('button')
-        highlight.setAttribute("id", "h" + number)
-        highlight.textContent = 'Highlight'
-        li.appendChild(highlight)
-
-        const span = document.createElement('span')
-        span.textContent = content
-        li.appendChild(span)
-        return li
-    },
-
-    handleDelete(event) {
-        event.preventDefault()
-        const itemContent = event.target.parentElement.querySelector('span').textContent
-        console.log(this.storedFlicks.length)
-        for (let i = 0; i < this.storedFlicks.length; i++) {
-            if (itemContent === this.storedFlicks[i]) {
-                this.storedFlicks.splice(i, 1)
-                break
-            }
-        }
-        event.target.parentElement.outerHTML = "";
-    },
-    handleHighlight(event) {
-        event.preventDefault()
-        if (event.target.parentElement.style.color == "green") {
-            event.target.parentElement.style.color = "black"
-            event.target.parentElement.style.fontFamily = "times"
-            event.target.parentElement.style.fontSize = "18px"
-        } else {
-            event.target.parentElement.style.color = "green"
-            event.target.parentElement.style.fontFamily = "Comic Sans MS"
-            event.target.parentElement.style.fontSize = "50px"
-        }
-    },
-    handleSwap(event) {
-        event.preventDefault()
-        const f = event.target
-        const indexOne = document.querySelector('#swapOne').value - 1
-        const indexTwo = document.querySelector('#swapTwo').value - 1
-        const temp = this.storedFlicks[indexOne]
-        this.storedFlicks[indexOne] = this.storedFlicks[indexTwo]
-        this.storedFlicks[indexTwo] = temp
-        this.reloadList()
+  addFlick(ev) {
+    ev.preventDefault()
+    const f = ev.target
+    const flick = {
+      id: this.max + 1,
+      name: f.flickName.value,
     }
 
+    this.flicks.unshift(flick)
+
+    const listItem = this.renderListItem(flick)
+    this.list
+      .insertBefore(listItem, this.list.firstChild)
+
+    ++ this.max
+    f.reset()
+  },
+
+  renderListItem(flick) {
+    const item = this.template.cloneNode(true)
+    item.classList.remove('template')
+    item.dataset.id = flick.id
+    item
+      .querySelector('.flick-name')
+      .textContent = flick.name
+
+    item
+      .querySelector('button.remove')
+      .addEventListener('click', this.removeFlick)
+
+    return item
+  },
+
+  removeFlick(ev) {
+    const listItem = ev.target.closest('.flick')
+    listItem.remove()
+  },
 }
 
 app.init({
-  formSelector: '#flickForm',
-  listSelector: '#flickList',
-  swapSelector: '#swapForm',
+  formSelector: '#flick-form',
+  listSelector: '#flick-list',
+  templateSelector: '.flick.template',
 })
-
-    $(function(){
-        var y = 0;
-        setInterval(function(){
-            y+=1;
-            $('body').css('background-position', y + 'px 0');
-        }, 10);
-    })
